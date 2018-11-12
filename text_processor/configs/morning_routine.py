9f3dd46d86d44ba6d467becee49570af5c836de0
Augusto Corvalan <augustocorvalan@gmail.com>
2018-11-11 19:16:36 -0500

    add morning routine

--- text_processor/configs/morning_routine.py
+++ text_processor/configs/morning_routine.py
@@ -1,3 +1,16 @@
-def get_config(model):
+from configs.default_config import Config
+from get_text import get_random_text
+
+class MorningConfig(Config):
     """ This config outputs the first two actions the agent takes each day """
-    pass
+    
+    header = "Mornings"
+
+    def GET_PARA_LIST(self, day: list) -> list:
+        """Only the first two things the agent does each day"""
+        sentence_list = []
+
+        for action in day[:2]:
+            sentence_list.append(get_random_text(action["action_name"]))
+
+        return sentence_list
--- text_processor/main.py
+++ text_processor/main.py
@@ -1,21 +1,27 @@
 from sample_model import model
 from get_text import get_random_text
 from configs.default_config import Config
+from configs.morning_routine import MorningConfig
 
 
-config = Config(model)
+# GOAL: output only the first two things the agent does. para=2 sentences. 
 
-days = config.FILTER_DAYS() # Example would be to only return first day
+def output_from_model(model, config_class):
+    config = config_class(model)
+
+    days = config.FILTER_DAYS() # Example would be to only return first day
+
+    section_list = []
+
+    for day in days:
+        para_list = []
+        character_day: list = day[config.GET_CHARACTER()]
+        para_list: list = config.GET_PARA_LIST(character_day)
+        section_list.append(para_list)
+
+    out = config.SECTION_TO_STR(section_list)
+
+    config.OUTPUT(out)
 
 
-section_list = []
-
-for day in days:
-  para_list = []
-  character_day: list = day[config.GET_CHARACTER()]
-  para_list: list = config.GET_PARA_LIST(character_day)
-  section_list.append(para_list)
-
-out = config.SECTION_TO_STR(section_list)
-
-config.OUTPUT(out)
+output_from_model(model, MorningConfig)
